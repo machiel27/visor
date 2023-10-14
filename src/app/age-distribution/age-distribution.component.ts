7b35c127037880766b22a569f90594d89412e946
Machiel <machielvanderwalt27@gmail.com>
2023-10-14 17:28:37 +0200

    Add age distribution and wins per country charts

--- src/app/age-distribution/age-distribution.component.ts
+++ src/app/age-distribution/age-distribution.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EChartsOption, SeriesOption } from 'echarts';
 
 @Component({
   selector: 'app-age-distribution',
   templateUrl: './age-distribution.component.html',
   styleUrls: ['./age-distribution.component.css']
 })
-export class AgeDistributionComponent {
+export class AgeDistributionComponent implements OnInit {
+  loading = true;
 
+  private baseUrl = 'https://localhost:7074/api/';
+
+  chartOption: EChartsOption = {
+    title: {
+      text: 'Age Distribution of F1 Championship Winners',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    xAxis: {
+      type: 'category',
+      data: ['18-25', '26-30', '31-35', '36-40', '41+']
+    },
+    yAxis: {
+      type: 'value',
+      name: 'Number of Winners'
+    },
+    series: [
+      {
+        name: 'Age Distribution',
+        type: 'bar',
+        data: [],
+        label: {
+          show: true,
+          position: 'top',
+          formatter: function (params) {
+            const winners = [
+              'List of winners for age group 18-25',
+              'List of winners for age group 26-30',
+              'List of winners for age group 31-35',
+              'List of winners for age group 36-40'
+            ];
+
+            return winners[params.dataIndex];
+          }
+        }
+      }
+    ]
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getAgeDistributionData();
+  }
+
+  getAgeDistributionData() {
+    this.http.get<any[]>(this.baseUrl + 'WinningAge').subscribe((data) => {
+      const ageGroups = [0, 0, 0, 0, 0];
+
+      data.forEach((item) => {
+        const age = item.age;
+
+        if (age >= 18 && age <= 25) {
+          ageGroups[0]++;
+        } else if (age >= 26 && age <= 30) {
+          ageGroups[1]++;
+        } else if (age >= 31 && age <= 35) {
+          ageGroups[2]++;
+        } else if (age >= 36 && age <= 40) {
+          ageGroups[3]++;
+        } else {
+          ageGroups[4]++;
+        }
+      });
+
+      (this.chartOption.series as SeriesOption[])[0].data = ageGroups;
+      this.loading = false;
+    });
+  }
 }
--- src/app/wins-per-country/wins-per-country.component.ts
+++ src/app/wins-per-country/wins-per-country.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EChartsOption, SeriesOption } from 'echarts';
 
 @Component({
   selector: 'app-wins-per-country',
   templateUrl: './wins-per-country.component.html',
   styleUrls: ['./wins-per-country.component.css']
 })
-export class WinsPerCountryComponent {
+export class WinsPerCountryComponent implements OnInit{
+  loading = true;
+
+  private baseUrl = 'https://localhost:7074/api/';
+  chartOption: EChartsOption = {
+    title: {
+      text: 'Championship wins per Country',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Wins From',
+        type: 'pie',
+        radius: '50%',
+        data: [],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getWinsPerCountry();
+  }
+
+  getWinsPerCountry() {
+    this.http.get<any[]>(this.baseUrl + 'NationalityWinCount').subscribe((data) => {
+      const formattedData = data.map((item) => {
+        return { value: item.wins, name: item.nationality };
+      });
+
+      (this.chartOption.series as SeriesOption[])[0].data = formattedData;
+      this.loading = false;
+    });
+  }
 
 }
